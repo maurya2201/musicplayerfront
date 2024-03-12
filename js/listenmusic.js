eab2b88c84cee844504ffa8c4afb8aba532e3d03
maurya2201 <mauryasoni71@gmail.com>
2024-03-13 00:26:10 +0530

    first commit

---
+++ js/listenmusic.js
@@ -0,0 +1,101 @@
+const urlParams = new URLSearchParams(window.location.search);
+const data =[];
+
+for(const param of urlParams){
+  data.push(param);
+}
+
+const[[key,value]]=data;
+const getOne=async()=>{
+  try{
+  const response = await axios.get(`http://localhost:4000/api/music/${value}`);
+  const {title,image,link,id} = response.data;
+  let musicPlayer =`
+  <div class="child">
+  <pre><h1>${title.toUpperCase()}</h1></pre>
+  <img alt="img" loading="lazy" src="${image}" height="300px" width="500px">
+  <div class="control">
+  <button class="btns" onclick="prev(${id})">⏮️</button>
+  <audio src="${link}" controls autoplay></audio>
+  <button class="btns" onclick="next(${id})">⏭️</button>
+  </div>
+  </div>
+  `;
+  document.getElementsByClassName("parent")[0].innerHTML=musicPlayer;
+  }catch(error){
+    console.log(error);
+  }
+}
+getOne();
+async function prev(id){
+  let prev = id-1;
+  if(prev<=0){
+    document.getElementById("prev").style.cursor="not-allowed";
+    document.getElementById("prev").style.backgroundColor="grey";
+    document.getElementById("prev").style.opacity="0.6";
+  }
+  else if(prev>=1){
+  document.getElementById("prev").style.cursor="pointer";
+  document.getElementById("prev").style.backgroundColor="black";
+  document.getElementById("prev").style.opacity="1";
+  const response = await axios.get(`http://localhost:4000/api/music`);
+  const filter = response.data.filter((element)=>element.id===prev);
+  let listing =``;
+  filter.map(({title,image,link,id})=>
+  listing +=`
+  <div class="child">
+  <pre><h1>${title.toUpperCase()}</h1></pre>
+  <img alt="img" loading="lazy" src="${image}" height="300px" width="500px">
+  <div class="control">
+  <button class="btns" onclick="prev(${id})">⏮️</button>
+  <audio src="${link}" controls autoplay></audio>
+  <button class="btns" onclick="next(${id})">⏭️</button>
+  </div>
+  </div>
+  `
+  );
+  document.getElementsByClassName("parent")[0].innerHTML=listing;
+  }
+}
+async function next(ids){
+  let next = ids+1;
+  const response = await axios.get(`http://localhost:4000/api/music`);
+  let max; 
+  for(let i=0;i<response.data.length;i++){
+    max = Math.max(response.data[i].id);
+  }
+  if(next>max){
+    const response = await axios.get(`http://localhost:4000/api/music/65f033e4f777594c54bad5f5`);
+    const {title,image,link,id} = response.data;
+    let musicPlayer =`
+    <div class="child">
+    <pre><h1>${title.toUpperCase()}</h1></pre>
+    <img alt="img" loading="lazy" src="${image}" height="300px" width="500px">
+    <div class="control">
+    <button class="btns" onclick="prev(${id})">⏮️</button>
+    <audio src="${link}" controls autoplay></audio>
+    <button class="btns" onclick="next(${id})">⏭️</button>
+    </div>
+    </div>
+    `;
+    document.getElementsByClassName("parent")[0].innerHTML=musicPlayer;
+  }
+  else{
+  const filter = response.data.filter((element)=>element.id===next);
+  let listing =``;
+  filter.map(({title,image,link,id})=>
+  listing +=`
+  <div class="child">
+  <pre><h1>${title.toUpperCase()}</h1></pre>
+  <img alt="img" loading="lazy" src="${image}" height="300px" width="500px">
+  <div class="control">
+  <button class="btns" onclick="prev(${id})">⏮️</button>
+  <audio src="${link}" controls autoplay></audio>
+  <button class="btns" onclick="next(${id})">⏭️</button>
+  </div>
+  </div>
+  `
+  );
+  document.getElementsByClassName("parent")[0].innerHTML=listing;
+  }
+}
---
+++ js/music.js
@@ -0,0 +1,28 @@
+const getMusic=async()=>{
+  try{
+  const response = await fetch(`http://localhost:4000/api/music/`);
+  const musics = await response.json();
+  let music =``;
+  musics.map(({title,image,_id})=>
+    music +=`
+    <tr>
+    <td>${title.toUpperCase()}</td>
+    <td><img src="${image}" alt="img" loading="lazy" height="150px" width="250px"></td>
+  <td>
+  <button class="music" onclick="listenMusic('${_id}')">▶️</button>
+  </td>
+    </tr>
+    `);
+  document.getElementsByTagName("tbody")[0].innerHTML=music;
+  }catch(error){
+    console.log(error);
+  }
+}
+getMusic();
+function listenMusic(id){
+  try{
+  window.location.href=`listenmusic.html?id=${id}`;
+  }catch(error){
+    console.log(error);
+  }
+}
